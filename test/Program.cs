b48d3f7995264fb8dd66bcb72e83deb7d9075f6f
holmen1 <holmen1@gmail.com>
2025-03-15 19:04:53 +0100

    refactor

--- test/Program.cs
+++ test/Program.cs
@@ -1,55 +1,64 @@
-﻿using System;
-using System.Linq;
+﻿using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using System.Text.Json;
 using test.Data;
 using test.Models;
-using System.Collections.Generic;
 
-class Program
+internal class Program
 {
-    static void Main()
+    private static void Main()
     {
         var configuration = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
             .AddJsonFile("appsettings.json")
             .Build();
-        
+
         using (var db = new ApplicationDbContext(configuration))
         {
+            if (!TableExists(db, "Assumptions")) db.Database.Migrate();
 
             if (!db.Assumptions.Any())
             {
-                var jsonFilePath = configuration["JsonSettings:FilePath"];
-                var jsonPropertyName = configuration["JsonSettings:PropertyName"];
-
-                var json = System.IO.File.ReadAllText(jsonFilePath);
-                var jsonData = JsonDocument.Parse(json).RootElement.GetProperty(jsonPropertyName);
-
-                var assumptions = new List<Assumption>();
-
-                foreach (var level in jsonData.EnumerateObject())
-                {
-                    foreach (var subLevel in level.Value.EnumerateObject())
-                    {
-                        foreach (var category in subLevel.Value.EnumerateObject())
-                        {
-                            assumptions.Add(new Assumption
-                            {
-                                Level = level.Name,
-                                SubLevel = subLevel.Name,
-                                Kategory = category.Name,
-                                Value = category.Value.GetDouble()
-                            });
-                        }
-                    }
-                }
-
+                var assumptions = LoadAssumptionsFromJson(configuration);
                 db.Assumptions.AddRange(assumptions);
                 db.SaveChanges();
             }
         }
+    }
+
+    private static bool TableExists(ApplicationDbContext context, string tableName)
+    {
+        var connection = context.Database.GetDbConnection();
+        connection.Open();
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = $"SELECT 1 FROM information_schema.tables WHERE table_name = '{tableName}'";
+            return command.ExecuteScalar() != null;
+        }
+    }
+
+
+    private static List<Assumption> LoadAssumptionsFromJson(IConfiguration configuration)
+    {
+        var jsonFilePath = configuration["JsonSettings:FilePath"];
+        var jsonPropertyName = configuration["JsonSettings:PropertyName"];
+
+        var json = File.ReadAllText(jsonFilePath);
+        var jsonData = JsonDocument.Parse(json).RootElement.GetProperty(jsonPropertyName);
+
+        var assumptions = new List<Assumption>();
+
+        foreach (var level in jsonData.EnumerateObject())
+        foreach (var subLevel in level.Value.EnumerateObject())
+        foreach (var category in subLevel.Value.EnumerateObject())
+            assumptions.Add(new Assumption
+            {
+                Level = level.Name,
+                SubLevel = subLevel.Name,
+                Kategory = category.Name,
+                Value = category.Value.GetDouble()
+            });
+
+        return assumptions;
     }
 }
